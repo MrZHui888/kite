05627066796e856c98611624d251982e587eff18
zhonghui.gu <zhonghui.gu>
2018-11-24 23:15:18 +0800

    sort

--- java/src/main/java/com/kite/algorithm/QuickSort.java
+++ java/src/main/java/com/kite/algorithm/QuickSort.java
@@ -9,11 +9,11 @@ import java.util.Arrays;
  */
 public class QuickSort {
     public static void main(String[] args) {
-        int[] num = { 2, 123, 43, 54, 654, 76, 2, 4, 33 };
+        int[] num = {2, 123, 43, 54, 654, 76, 2, 4, 33};
 
         sort(num, 0, num.length - 1);
 
-       Arrays.stream(num).forEach(x->System.out.print(x+" "));
+        Arrays.stream(num).forEach(x -> System.out.print(x + " "));
 
     }
 
@@ -25,23 +25,18 @@ public class QuickSort {
         int i = left;
         int j = right;
         while (i != j) {
-            while (i < j && numList[i] <= tmp) {
-                i++;
-                if (i < j) {
-                    numList[j] = numList[i];
-                }
-            }
-
-            while (i < j && numList[j] > tmp) {
+            while (i < j && numList[j] >= tmp)
                 j--;
-                if (j > i) {
-                    numList[i] = numList[j];
-                }
-            }
+            if (j > i)
+                numList[i] = numList[j];//a[i]已经赋值给temp,所以直接将a[j]赋值给a[i],赋值完之后a[j],有空位
+            while (i < j && numList[i] <= tmp)
+                i++;
+            if (i < j)
+                numList[j] = numList[i];
         }
         numList[i] = tmp;
         sort(numList, left, i - 1);
-        sort(numList, i+1, right);
+        sort(numList, i + 1, right);
     }
 
 }
