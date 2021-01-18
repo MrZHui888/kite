7e9aa0f06de94fa3138b2a53438fb015caa8188e
guzhonghui <guzhonghui@58.com>
2021-01-18 17:32:22 +0800

    fix threadPool

--- java/src/main/java/com/disruptor/bhz/threadpoolv1/core/SequenceDistributor.java
+++ java/src/main/java/com/disruptor/bhz/threadpoolv1/core/SequenceDistributor.java
@@ -7,8 +7,10 @@ import com.lmax.disruptor.dsl.Disruptor;
  * 顺序选择器 根据序号
  */
 public class SequenceDistributor implements Distributor {
+
     @Override
     public Disruptor<RunEvent> selectOne(Disruptor<RunEvent>[] disruptors, int splitSize, int splitFactor, long sequence, boolean endOfBatch) {
         return disruptors[(int) (sequence & splitFactor)];
     }
+
 }
--- java/src/main/java/com/disruptor/bhz/threadpoolv1/core/ThreadFactorys.java
+++ java/src/main/java/com/disruptor/bhz/threadpoolv1/core/ThreadFactorys.java
@@ -3,6 +3,7 @@ package com.disruptor.bhz.threadpoolv1.core;
 import java.util.concurrent.ThreadFactory;
 
 public class ThreadFactorys {
+
     static final ThreadFactory DAEMON_THREAD_FACTORY = (r)-> {
         Thread t = new Thread(r);
         t.setName("threadpool.daemon");
