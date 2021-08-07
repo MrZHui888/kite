370f462a0c185c12760590a3d23087168eb1870b
guzhonghui <guzhonghui@58.com>
2021-08-08 00:00:57 +0800

    新增316

--- java/src/main/java/com/kite/algorithm/Solution316.java
+++ java/src/main/java/com/kite/algorithm/Solution316.java
@@ -4,6 +4,9 @@ package com.kite.algorithm;
 import java.util.ArrayDeque;
 import java.util.Deque;
 
+/**
+ * @author gzh
+ */
 public class Solution316 {
     public static void main(String[] args) {
         String s = "bcabc";
@@ -15,17 +18,33 @@ public class Solution316 {
     public String removeDuplicateLetters(String s) {
         char[] chars = s.toCharArray();
         int length = s.length();
-        Deque<Character> stack = new ArrayDeque<>();
-        boolean[] vis = new boolean[26];
         int[] lastIndex = new int[26];
         for (int i = 0; i < length; i++) {
-            if (stack.isEmpty()) {
-                stack.add(chars[i]);
-            }
-            Character peek = stack.peek();
+            lastIndex[chars[i] - 'a'] = i;
         }
-        return "";
+
+        Deque<Character> stack = new ArrayDeque();
+        boolean[] visited = new boolean[26];
+        for (int i = 0; i < length; i++) {
+            char c = chars[i];
+            // 1. 先看没有的
+            // 1.查看栈里边不为空
+            //2.栈头里边的数小于c
+            if (!visited[c - 'a']) {
+                //若栈为空，或当前元素大于栈顶元素，或栈顶元素在当前字符后不再出现，当前字符压栈
+                while (!stack.isEmpty() && c < stack.peek() &&
+                        lastIndex[stack.peek() - 'a'] > i) {
+                    visited[stack.pop() - 'a'] = false;
+                }
+                visited[c - 'a'] = true;
+                stack.push(c);
+            }
+
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        return sb.reverse().toString();
     }
-
-
 }
